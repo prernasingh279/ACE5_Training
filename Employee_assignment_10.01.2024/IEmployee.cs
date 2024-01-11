69ee2191282746fe02414c851fa2dcc39b658609
prernasingh279 <75928547+prernasingh279@users.noreply.github.com>
2024-01-11 16:29:41 +0530

    Add files via upload

---
+++ Employee_assignment_10.01.2024/IEmployee.cs
@@ -0,0 +1,9 @@
+using System;
+namespace I{
+    public interface IEmployee
+    {
+        public void AcceptDetails();
+        public void CalculateSalary();
+        public void DisplayDetails();
+    }
+}
---
+++ Employee_assignment_10.01.2024/IEmployee_client.cs
@@ -0,0 +1,24 @@
+namespace I{
+    class IEmployeeClient
+    {
+            public static void Main(){
+            Employee e1  = new Permanent();
+            Employee t1 = new Trainee();
+            System.Console.WriteLine("Enter 1 for Permanent and 2 for Trainee");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if(n==1)
+            {
+            e1.AcceptDetails();
+            e1.CalculateSalary();
+            e1.DisplayDetails();
+            }
+           
+           else{
+              t1.AcceptDetails();
+            t1.CalculateSalary();
+            t1.DisplayDetails();
+           }
+
+           }
+    }
+}
